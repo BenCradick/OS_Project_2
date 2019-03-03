0fdca341ec19bb2b4f3b521f62658b75279c61cb
bencradick <ben.cradick@gmail.com>
2019-03-03 12:55:01 -0600

    Spawns everything correctly hand handles all program requirements. Now I will write the makefile.

--- child.c
+++ child.c
@@ -4,25 +4,58 @@
 
 #include <stdio.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <limits.h>
 
-int main(){
+void checkErrorFlag(char* string);
 
+int main(int argc, char **argv){
+    char cwd[PATH_MAX];
+
+    getcwd(cwd, sizeof(cwd));
+   // printf("%s\n",cwd);
     key_t key = ftok("oss.c", 42);
+    key_t key2 = ftok("child.c", 76); // again arbitrary
+    checkErrorFlag("ftok() failed");
 
-    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+    int shmid = shmget(key, sizeof(int),  0666 | IPC_CREAT);
+    int shmid2  = shmget(key2, sizeof(int), 0666 | IPC_CREAT);
 
-    char* str = (char*) shmat(shmid, (void*)0,0);
+    long* sec = (long*) shmat(shmid, NULL,0);
+    long* nano = (long*) shmat(shmid2, NULL, 0);
 
-    printf("%s\n", str);
+    long duration = strtol(argv[1], NULL, 10);
 
-    shmctl(shmid, IPC_RMID, NULL);
-    if(errno != 0){
-        perror("Error reading shared memory: ");
-        return EXIT_FAILURE;
+    long endSec;
+    long endNano;
+
+    const long billion = 1000000000;
+
+    long temp = duration % billion;
+
+    endSec = *sec + temp;
+    endNano = *nano - (temp * billion);
+
+    while(*sec <= endSec && *nano <= endNano){
+//        sec = (long*)shmat(shmid, NULL, 0);
+//        nano = (long*)shmat(shmid2, NULL, 0);
     }
-    return 0;
+
+    fprintf(stdout, "pid: %d secounds: %ld nanoseconds: %ld %s\n", getpid(), *sec, *nano, "Child process is terminating.");
+    shmdt(sec);
+    shmdt(nano);
+    exit(EXIT_SUCCESS);
+}
+void checkErrorFlag(char* str){
+    if(errno != 0){
+        perror(str);
+        exit(EXIT_FAILURE);
+    }
+    else{
+        return;
+    }
 }
--- oss.c
+++ oss.c
@@ -7,22 +7,62 @@
 #include <limits.h>
 #include <getopt.h>
 #include <ctype.h>
-#include <time.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <memory.h>
 
 void checkErrorFlag(char* str);
+void sigHandle(int sig);
+void programAlarm(void);
+void freeSharedMemory();
+void incrementTime(long *nano, long *sec, long timeIncrement, long billion);
+void printSharedClock();
+
+
+FILE *outputStream; // needed to be global for requirement about outputing shared clock data.
 
 int main(int argc, char **argv) {
+
+
+    struct itimerval it_val;
+
+    if(signal(SIGALRM, (void(*)(int)) programAlarm) == SIG_ERR){
+        perror("Failed to catch SIGALRM");
+        exit(1);
+    }
+    if(signal(SIGABRT,sigHandle) == SIG_ERR){
+        perror("Failed to catch SIGABRT\n");
+    }
+    if(signal(SIGINT, sigHandle) == SIG_ERR){
+        perror("Failed to catch SIGINT\n");
+    }
+    it_val.it_value.tv_sec = 10;
+    it_val.it_value.tv_usec = 0;
+
+    it_val.it_interval = it_val.it_value;
+
+    if (setitimer(ITIMER_REAL, &it_val, NULL) == -1) {
+        perror("Failed to call setitimer: ");
+        exit(1);
+    }
     /*
      * Variables used for handling shared memory -Start
      */
-    key_t key = ftok("oss.c",42); //42 is arbitrary, but also the answer to life the universe and everything.
-    key_t key2 = ftok("oss.c", 76); // again arbitrary
+    key_t key = ftok("oss.c", 42); //42 is arbitrary, but also the answer to life the universe and everything.
+    checkErrorFlag("Error generating shared memory keys: ");
+    key_t key2 = ftok("child.c", 76); // again arbitrary
+    checkErrorFlag("Error generating shared memory keys: ");
 
-    int shmid = shmget(key, sizeof(int),0666 |IPC_CREAT);
+    int shmid = shmget(key, sizeof(int),  0666 | IPC_CREAT);
     int shmid2  = shmget(key2, sizeof(int), 0666 | IPC_CREAT);
+    checkErrorFlag("Error assigning shared memory id: ");
 
-    int* sec = (int*) shmat(shmid, NULL,0);
-    int* nano = (int*) shmat(shmid2, NULL, 0);
+    long* sec = (long*) shmat(shmid, NULL,0);
+    long* nano = (long*) shmat(shmid2, NULL, 0);
+
+    FILE *inputStream;
+
+    checkErrorFlag("Error assigning shared memory to variable: ");
     /*
      * Variables used for shared memory -End
      *
@@ -30,27 +70,37 @@ int main(int argc, char **argv) {
      */
 
     int c; // used to hold return value of getopt
-    int timeIncrament; // specified by first line of input file
+    long timeIncrement; // specified by first line of input file
 
-    long maxChildren;
-    long maxLiveChildren;
+    long nextSec;
+    long nextNano;
+
+    long maxChildren = 4;
+    long maxLiveChildren = 2;
+    long liveChildren = 0;
+    long children = 0;
+    const long billion = 1000000000;
 
     char* inputFile;
     char* outputFile;
 
     char line[LINE_MAX];
+    char* tokens[3];
+    char* token;
+
+    int i = 0; // iterator
+    int status;
+
+    pid_t pid;
 
 
-    FILE *inputStream;
-    FILE *outputStream;
 
     //Default values for input/output files
     inputFile = "input.txt";
     outputFile = "output.txt";
     // End argument handling variables.
 
-    time_t start;
-    time(&start);
+    checkErrorFlag("Error fetching the time: ");
 
 
     while((c = getopt(argc, argv, "h:n:s:i:o:")) != -1){
@@ -70,6 +120,9 @@ int main(int argc, char **argv) {
                 break;
             case 's':
                 maxLiveChildren = strtol(optarg, NULL, 10);
+                if(maxLiveChildren >=20){
+                    maxLiveChildren = 19;
+                }
                 break;
             case 'i':
                 inputFile = optarg;
@@ -87,11 +140,11 @@ int main(int argc, char **argv) {
                 else{
                     fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
                 }
-                abort();
+                exit(EXIT_FAILURE);
             default:
                 fprintf(stderr, "Unknown command line argument occurred.\n");
                 fprintf(stdout, "Use commandline argument -h for help\n");
-                abort();
+                exit(EXIT_FAILURE);
         }
     }
 
@@ -108,29 +161,172 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    timeIncrament = strtol(line, NULL, 10);
-
-
-
-
-    shmdt(sec);
-
-    if(errno != 0){
-        perror("Unable to partition shared memory: ");
-        return EXIT_FAILURE;
+    timeIncrement = strtol(line, NULL, 10);
+    fgets(line, LINE_MAX, inputStream);
+    token = strtok(line, " ");
+    while(token != NULL){
+        tokens[i] = token;
+        token = strtok(NULL, " ");
+        i++;
     }
 
+    i = 0; //reset for later use.
+
+    nextSec = strtol(tokens[0], NULL, 10);
+    nextNano = strtol(tokens[1], NULL, 10);
+
+
+    while(children < maxChildren){
+        incrementTime(nano, sec, timeIncrement, billion);
+        if(liveChildren < maxLiveChildren && *sec >= nextSec && *nano >= nextNano){
+            liveChildren++;
+            children++;
+            fprintf(outputStream, "seconds: %ld nanoseconds: %ld pid: %d duration: %s", *sec, *nano, getpid(), tokens[2]);
+            fflush(outputStream); // without flush child prints as well
+
+            pid = fork();
+            if(pid == 0){
+
+
+                fclose(outputStream);
+                char* args[3] = {"./child", tokens[2], (char*)0}; // needed null terminator
+                if(execv("./child", args) == -1){
+                    perror("Error executing file");
+                }
+            }
+            else{
+
+                if(fgets(line,LINE_MAX, inputStream) != NULL){
+                    token = strtok(line, " ");
+                    while( token != NULL){
+                        tokens[i] = token;
+                        token = strtok(NULL, " ");
+                        i++;
+                    }
+                    nextSec = strtol(tokens[0],NULL,10);
+                    nextNano = strtol(tokens[1], NULL, 10);
+                    i = 0; // reset again
+                }
+                else{
+                    children = maxChildren;
+
+                }
+            }
+        }
+
+        if((pid = waitpid(0, &status, WNOHANG)) > 0){
+            if(WIFEXITED(status)) {
+                liveChildren--;
+                fprintf(outputStream, "pid: %d seconds: %ld nanoseconds: %ld\n", pid, *sec, *nano);
+            }
+
+
+        }
+            if(errno > 0)
+            {
+                if(errno == ECHILD)
+                {
+                    errno = 0;
+                }
+                else
+                {
+                    perror("wait error");
+                }
+            }
+
+    }
+    while(liveChildren > 0){
+        incrementTime(nano, sec, timeIncrement, billion);
+
+        if((pid = waitpid(0, &status, WNOHANG)) > 0){
+            if(WIFEXITED(status)) {
+                fprintf(outputStream, "pid: %d seconds: %ld nanoseconds: %ld\n", pid, *sec, *nano);
+                liveChildren--;
+
+            }
+
+        }
+        else{
+            if(errno == ECHILD){
+                liveChildren = 0;
+            }
+        }
+    }
+    shmdt(sec);
+    shmdt(nano);
+
+    printSharedClock();
+    freeSharedMemory();
+    fclose(outputStream);
     return EXIT_SUCCESS;
 }
 
 
 //helper for streamlining the checking for the error flag and then writing an error message.
 void checkErrorFlag(char* str){
-    if(!errno){
+    if(errno != 0){
         perror(str);
-        abort();
+        exit(EXIT_FAILURE);
     }
     else{
         return;
     }
+}
+void sigHandle(int sig) {
+
+
+    if (errno == ECHILD) {
+        errno = 0;
+    }
+    printSharedClock();
+    freeSharedMemory();
+    exit(EXIT_FAILURE);
+}
+void programAlarm(){
+    fprintf(stdout,"Program time out\n");
+    signal(SIGINT, SIG_IGN);
+    kill(0, SIGINT);
+    printSharedClock();
+    freeSharedMemory();
+    exit(EXIT_FAILURE);
+
+
+}
+void freeSharedMemory(){
+
+    key_t key = ftok("oss.c",42); //42 is arbitrary, but also the answer to life the universe and everything.
+    key_t key2 = ftok("child.c", 76); // again arbitrary
+    checkErrorFlag("Error generating shared memory keys: ");
+
+    int shmid = shmget(key, sizeof(int), 0666 | IPC_CREAT);
+    int shmid2  = shmget(key2, sizeof(int), 0666 | IPC_CREAT);
+    checkErrorFlag("Error assigning shared memory id: ");
+
+    shmctl(shmid,IPC_RMID,NULL);
+    shmctl(shmid2, IPC_RMID,NULL);
+}
+void incrementTime(long *nano, long *sec, long timeIncrement, const long billion){
+    *nano = timeIncrement + *nano;
+    if(*nano > billion){
+        long temp = *nano / billion;
+        *sec += temp;
+        *nano = *nano % billion;
+    }
+}
+void printSharedClock(){
+    key_t key = ftok("oss.c",42); //42 is arbitrary, but also the answer to life the universe and everything.
+    key_t key2 = ftok("child.c", 76); // again arbitrary
+    checkErrorFlag("Error generating shared memory keys: ");
+
+    int shmid = shmget(key, sizeof(int), 0666 | IPC_CREAT);
+    int shmid2  = shmget(key2, sizeof(int), 0666 | IPC_CREAT);
+
+    long* sec = (long*) shmat(shmid, NULL,0);
+    long* nano = (long*) shmat(shmid2, NULL, 0);
+
+    fprintf(outputStream, "seconds: %ld nanoseconds: %ld\n", *sec, *nano);
+
+    shmdt(sec);
+    shmdt(nano);
+
 }
